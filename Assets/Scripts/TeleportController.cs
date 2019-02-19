121e16b3faef2cf482ca8ac3a53a002600c9b7f4
Solomon <solomon@somosoft.com>
2019-02-19 15:47:23 -0500

    initial commit

---
+++ Assets/Scripts/TeleportController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour
+{
+
+    public void SetGazedAt(bool gazedAt) 
+    {
+        if(gazedAt == true)
+        {
+            GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        }
+        else
+        {
+            GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetGazedAt(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
