f5e08bfda20a40b5e1e43f70dfff863bb5ed3985
Solomon <solomon@somosoft.com>
2019-02-19 15:59:40 -0500

    Implemented teleport feature

--- Assets/Scripts/TeleportController.cs
+++ Assets/Scripts/TeleportController.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class TeleportController : MonoBehaviour
 {
+    private bool isTargeted;
+    public float playerHeight = 1.8f;
+
+    public void Teleport()
+    {
+        if (isTargeted)
+        {
+            // Player is looking at this target
+            // when they clicked their headset.
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player != null)
+            {
+                // move the player to the location of the target
+                Vector3 newPosition = new Vector3(transform.position.x,
+               transform.position.y + playerHeight,
+                transform.position.z);
+                player.transform.position = newPosition;
+            }
+        }
+    }
 
     public void SetGazedAt(bool gazedAt) 
     {
         if(gazedAt == true)
         {
             GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+
+            isTargeted = gazedAt;
         }
         else
         {
